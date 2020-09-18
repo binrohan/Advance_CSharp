4b4b0c2c406109853303c662df20feb9eaa16b17
Rohan-Bin-Khokon <binrohan97@gmail.com>
2020-09-18 22:07:38 +0600

    Extension Methods

---
+++ Extension Methods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Extension_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post =  "This is supposed to be a very long blog post";
+            var shortenPost = post.Shorten(6);
+
+            System.Console.WriteLine(shortenPost + "....");
+        }
+    }
+}
