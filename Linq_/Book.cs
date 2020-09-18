3f6cfadeef7a9cd4cc156495076149a80de0ca65
Rohan-Bin-Khokon <binrohan97@gmail.com>
2020-09-18 23:55:45 +0600

    Linq

---
+++ Linq_/Book.cs
@@ -0,0 +1,8 @@
+namespace Linq_
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ Linq_/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books =  new BookRepository().GetBooks();
+
+            //Linq Query Operator
+            var cheaperBooks = 
+                from b in books
+                where b.Price < 10
+                orderby b.Title
+                select b;
+
+            // Linq Extension Methods
+            var cheapBooks = books
+                                .Where(b => b.Price < 10)
+                                .OrderBy(b => b.Title)
+                                .Select(b => b.Title);
+
+            var bookQuery = books.Single(b => b.Title == "ASP.Net MVC");
+            var bookQuerySingleOrDefault = books.SingleOrDefault(b => b.Title == "ASP.Net");
+
+            var bookQueryFirst =  books.First(b => b.Title == "C# Advanced Topics");
+            var bookQueryFirstOrDefault =  books.FirstOrDefault(b => b.Title == "C# Advanced");
+
+            System.Console.WriteLine("First or Default " + bookQueryFirstOrDefault);
+            System.Console.WriteLine("First " + bookQueryFirst.Price);
+
+            var bookQueryLast =  books.Last(b => b.Title == "C# Advanced Topics");
+            var bookQueryLastOrDefault =  books.LastOrDefault(b => b.Title == "C# Advanced");
+
+            System.Console.WriteLine("Last or Default " + bookQueryLastOrDefault);
+            System.Console.WriteLine("Last " + bookQueryLast.Price);
+
+            System.Console.WriteLine("Single" + bookQuery.Title);
+            if(bookQuerySingleOrDefault != null)
+                System.Console.WriteLine("Single Or Default " + bookQuerySingleOrDefault.Title);
+
+            foreach (var book in cheapBooks)
+            {
+                System.Console.WriteLine(book);
+            }
+            foreach (var book in cheaperBooks)
+            {
+                System.Console.WriteLine(book.Title + " " + book.Price);
+            }
+
+            // Skip and Take
+            System.Console.WriteLine("Skip and Take");
+            var bookQuerySkipAndTake =  books.Skip(2).Take(2);
+            foreach (var book in bookQuerySkipAndTake)
+            {
+                System.Console.WriteLine(book.Title + " " + book.Price);
+            }
+
+            var count = books.Count();
+            System.Console.WriteLine("Number of books in repository " + count);
+
+            var maxPrice = books.Max(b => b.Price);
+            var minPrice = books.Min(b => b.Price);
+            System.Console.WriteLine("Max price " + maxPrice + "\n" + "Min Price " + minPrice);
+
+            var totalPrice = books.Sum(b => b.Price);
+            System.Console.WriteLine("Total Price " + totalPrice);
+            
+            var averagePrice = books.Average(b => b.Price);
+            System.Console.WriteLine("Average Price " + averagePrice);
+        }
+    }
+}
