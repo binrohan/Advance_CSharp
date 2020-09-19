b38b6334bdc83df955a745d8c250e52f89f60e68
Rohan-Bin-Khokon <binrohan97@gmail.com>
2020-09-19 21:45:24 +0600

    Dynamic

---
+++ Dynamic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic name = "my name";
+            name = 10;
+
+            dynamic a = 10;
+            dynamic b = 5;
+            var c = a + b;
+
+            int i = 5;
+            dynamic d = i;
+
+            long l = d;
+
+
+            
+        }
+    }
+}
