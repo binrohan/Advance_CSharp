7979b3e95da8f60f8145529d1dd7083118d87b41
Rohan-Bin-Khokon <binrohan97@gmail.com>
2020-09-20 15:30:08 +0600

    Generics

---
+++ Generics/BookList.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Generics
+{
+    public class BookList
+    {
+        public void Add(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book { Isbn = "1111", Title = "C# Advanced" };
+
+            var numbers = new List();
+            // numbers.Add(10);
+
+            var books = new BookList();
+            // books.Add(book);
+
+            var Gnumbers = new GenericList<int>();
+            // Gnumbers.Add(10);
+
+            var Gbooks = new GenericList<Book>();
+            // books.Add(new Book());
+
+            var dictionary = new GenericDictionary<String, Book>();
+            // dictionary.Add("123", book);
+
+            var number = new Nullable<int>();
+            System.Console.WriteLine("Has Value ? " + number.HasValue);
+            System.Console.WriteLine("Value: " + number.GetValueOrDefault());
+        }
+    }
+}
+ 
